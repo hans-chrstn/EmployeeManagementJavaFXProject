1aa2148382324e919d3919f5e46cfc8c2c4eac0f
Afton Goulding <aftografto@gmail.com>
2026-04-21 16:22:57 -0400

    fix: database credentials error message

--- app/src/main/java/dev/project/employeemanagement/repository/DbConfig.java
+++ app/src/main/java/dev/project/employeemanagement/repository/DbConfig.java
@@ -7,14 +7,16 @@ import java.sql.SQLException;
 public class DbConfig {
   private static final String URL = "jdbc:mysql://localhost:3306/employee_db";
 
-  // you need a .env or set your export DB_USER=youruser and DB_PASS=yourpass setup that is
+  // you need a .env or set your export DB_USER=youruser and DB_PASS=yourpass
+  // setup that is
   // connected to your db
   public static Connection getConnection() throws SQLException {
     String user = System.getenv("DB_USER");
     String pass = System.getenv("DB_PASS");
 
     if (user == null || pass == null) {
-      throw new SQLException("Database credentials not found. Ensure .env is loaded LOL");
+      throw new SQLException(
+          "Database credentials not found. Please set DB_USER and DB_PASS in your terminal before running.");
     }
 
     return DriverManager.getConnection(URL, user, pass);
